8b953b6622f9ae6be66c02899134831b61276a42
kukuhtri1999 <kukuhthewow@gmail.com>
2024-08-09 19:21:10 +0700

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import vue from '@vitejs/plugin-vue';
 import vuetify from 'vite-plugin-vuetify';
 
 export default defineConfig({
+  //   server: {
+  //     hmr: {
+  //       host: 'localhost',
+  //     },
+  //   },
+  //   server: {
+  //     host: 'localhost', // Add this to force IPv4 only
+  //     port: 5173,
+  //   },
   plugins: [
     laravel({
       input: 'resources/js/app.js',
